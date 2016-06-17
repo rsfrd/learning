b997dab701433c882da4f04455e0c046f6cdd9cb
rsfrd <matt.ransford@gmail.com>
2016-06-17 13:38:45 -0700

    new lessons

--- ex33.py
+++ ex33.py
@@ -1,16 +1,28 @@
-i = 0
-numbers = []
 
-while i < 6:
-    print "At the top i is %d" % i
-    numbers.append(i)
+def f(numbers):
+    for i in range(0, 6): 
+        print "At the top i is %d" % i
+        numbers.append(i)
 
-    i = i + 1
-    print "Numbers now: ", numbers
-    print "At the bottom i is %d" % i
+        print "Numbers now: ", numbers
+        print "At the bottom i is %d" % i
+        f(numbers)
 
-print "The numbers: "
+numbers = f([])
 
 for num in numbers:
     print num
 
+# while i < 6:
+#     print "At the top i is %d" % i
+#     numbers.append(i)
+# 
+#     i = i + 1
+#     print "Numbers now: ", numbers
+#     print "At the bottom i is %d" % i
+#  
+# print "The numbers: "
+#  
+# for car in numbers:
+#     print car 
+#  
